fb0077872abfcb1269820464b29e61685726ebbc
Bill Rossi <bassguitarbill@gmail.com>
2023-03-05 13:19:27 -0500

    Try to get yew set up

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,32 @@ mod numbers;
 mod verbs;
 
 use numbers::numbers_test;
+use yew::prelude::*;
+use yew::ChangeData;
 
-use crate::verbs::{ run_verb_test };
+#[function_component]
+fn App() -> Html {
+    let query = use_state(|| "");
+    let onchange = {
+	let query = query.clone();
+	Callback::from(move |event: ChangeData| {
+	    match event {
+		ChangeData::Value(s) => { query.set(input.to_string());}
+		_ => {}
+	    }
+	})
+    };
+    
+    html! {
+	<div>
+	    <div>{"こんにちは日本語！"}</div>
+	    <input type="text" value={query} onchange={onchange} />
+	</div>
+    }
+}
 
 fn main() {
-    //run_verb_test();
-    numbers_test();
+    // run_verb_test();
+    // numbers_test();
+    yew::Renderer::<App>::new().render();
 }
